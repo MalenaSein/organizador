c8f2690b44a4c5a0aac025511061eeee4ecae5a7
Malenit <msein@fi.uba.ar>
2026-03-02 11:40:10 -0300

    agrego service worker y manifest para que la app sea instalable

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const CACHE = 'organizador-v1';
+const ASSETS = [
+  '/organizador/',
+  '/organizador/index.html',
+  '/organizador/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
